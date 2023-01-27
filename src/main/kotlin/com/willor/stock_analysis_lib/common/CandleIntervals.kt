ca5306009d36207ed326863bab0cd4ad3ac6eb7c
willor777 <wcorwig777@gmail.com>
2023-01-27 17:06:16 -0500

    analysis package updated

--- src/main/kotlin/com/willor/stock_analysis_lib/common/CandleIntervals.kt
+++ src/main/kotlin/com/willor/stock_analysis_lib/common/CandleIntervals.kt
@@ -4,11 +4,12 @@ enum class CandleInterval(
     val timeOfFirstCandle: String,
     val hourOfFirstCandle: Int,
     val minuteOfFirstCandle: Int,
+    val serverCode: String
     ) {
-    M1("09:31", 9, 31),
-    M5("09:35", 9, 35),
-    M15("09:45", 9, 45),
-    M30("10:00", 10, 0),
-    H1("10:30", 10, 30),
-    D1("09:30", 9, 30),
+    M1("09:31", 9, 31, "1m"),
+    M5("09:35", 9, 35, "5m"),
+    M15("09:45", 9, 45, "15m"),
+    M30("10:00", 10, 0, "30m"),
+    H1("10:30", 10, 30, "1h"),
+    D1("09:30", 9, 30, "1d"),
 }
